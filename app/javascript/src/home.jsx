e354e97ccd04f99d8211264d261d526fa54ec04b
sofiacmachado <sofia_cmachado@hotmail.com>
2022-09-12 21:26:02 +0100

    Mybooks

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -4,21 +4,25 @@ import Layout from './layout';
 import './home.scss';
 import heroBackground from "/app/assets/images/hero_background.png";
 
-const Home = () => (
-  <Layout>
-    <div className="intro-section" style={{ backgroundImage: `url(${heroBackground})`}}>
-        <div className="container">
-            <div className="row align-items-center justify-content-center">
-                <div className="col-md-7 mx-auto text-center slogan-text" data-aos="fade-up">
-                    <h1>Discover your next read</h1>
-                    <p><a href="#">Browse</a> through our list and find books to fill your shelves.</p>
-                    <p><a href="#">Sell</a> your already read books to someone who wants to read it.</p>
+class Home extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div className="intro-section" style={{ backgroundImage: `url(${heroBackground})`}}>
+            <div className="container">
+                <div className="row align-items-center justify-content-center">
+                    <div className="col-md-7 mx-auto text-center slogan-text" data-aos="fade-up">
+                        <h1>Discover your next read</h1>
+                        <p><a href="#">Browse</a> through our list and find books to fill your shelves.</p>
+                        <p><a href="#">Sell</a> your already read books to someone who wants to read it.</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  </Layout>
-)
+      </Layout>
+    )
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
