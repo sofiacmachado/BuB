99a054a285a294da763097ec1011524ca2002e8c
sofiacmachado <sofia_cmachado@hotmail.com>
2022-09-14 11:24:45 +0100

    book item shows

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
+
   get '/mybooks' => 'static_pages#mybooks'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/mybooks/add' => 'static_pages#add'
+  get '/book/:id' => 'static_pages#book'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([
+  { username: 'Tommy', email: 'tommy@test.com', password: 'password', first_name: 'Tommy', last_name: 'Smith', address:'Street X NY', cellphone: 000000001,  },
+  { username: 'Bobby', email: 'bobby@test.com', password: 'password', first_name: 'Bobby', last_name: 'Peterson', address:'Street Y ST', cellphone: 000000002, },
+  { username: 'Sarah', email: 'sarah@test.com', password: 'password', first_name: 'Sarah', last_name: 'Gardner', address:'Street Z FL', cellphone: 000000003, },
+])
+
+books = Book.create([
+    {
+    title: 'Notes from Underground',
+    author: "Fyodor Dostoevsky",
+    isbn: '9780679734529',
+    description: "Notes from Underground is a novella written in 1864 by Fyodor Dostoevsky, and is considered by many to be one of the first existentialist novels. The novella presents itself as an excerpt from the rambling memoirs of a bitter, isolated, unnamed narrator, who is a retired civil servant living in St. Petersburg.",
+    condition: "Like new",
+    user_description: 'Book is in perfect conditions',
+    genre: 'classic',
+    price: 8,
+    rating: 4.2/5,
+    image_url: 'https://images-na.ssl-images-amazon.com/images/I/41kxGhOH0vL._SX322_BO1,204,203,200_.jpg',
+    user: users.first
+    }
+])
