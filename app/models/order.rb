50df32b96ca0c323a6ab7b899722ed500eebedfb
sofiacmachado <sofia_cmachado@hotmail.com>
2022-10-22 07:56:04 +0100

    change order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,10 @@
 class Order < ApplicationRecord
     belongs_to :user
     belongs_to :book
-    belongs_to :cart
     has_many :charges 
   
     validates :user, presence: true
     validates :book, presence: true
-    validates :cart, presence: true
   
     end
   end
