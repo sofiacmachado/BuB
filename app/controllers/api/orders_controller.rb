91ff9c8e372ced6d3fc78b2573556f072e7fb511
sofiacmachado <sofia_cmachado@hotmail.com>
2022-10-28 15:08:23 +0100

    working stripe after error

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -58,7 +58,7 @@ module Api
       token = cookies.signed[:bub_session_token]
       @session = Session.find_by(token: token)
 
-      if session
+      if @session
         @user = @session.user
       else
         render json: { error: 'user not logged in' }, status: :unauthorized
